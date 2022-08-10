a5aa1b08a5b15294470d3684ca9e0a07ef9c8d20
root <denny4009@gmail.com>
2022-08-10 15:49:02 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('admin/', admin.site.urls),
+    path('',views.index),
 ]
