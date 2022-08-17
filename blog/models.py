85d4a02d2dc7288b68dc18658a725f37df2027e1
root <denny4009@gmail.com>
2022-08-17 09:38:53 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,10 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
     def __str__(self):
         return self.name
-class Meta:
-    verbose_name_plural = 'Categories'
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    class Meta:
+        verbose_name_plural = 'Categories'
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100,blank=True)
--- blog/urls.py
+++ blog/urls.py
@@ -22,6 +22,7 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.PostList.as_view()),
-    path('<int:pk>/',views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.category_page),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
 ]
